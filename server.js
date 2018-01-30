5f747c7549f47823137caa63bb6222d4a58a7a8f
Mahbub Alam <makjoybd@gmail.com>
2018-01-30 12:42:10 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} - ${req.url}`;
+
+    fs.appendFile('server.log', log + "\r\n", (err) => {
+        console.log("unable to write to log file");
+    });
+
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: "Welcome to this awesome page!"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+})
+
+app.listen(3000);
